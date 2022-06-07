a1bd13fa55fa67ccc1b66b5986b35c1c370e5e3f
Kirill Elizarov <kirillthetee@gmail.com>
2022-06-07 14:12:17 +0700

    Adds support for login via cardano light wallets (Nami, Flint).

--- packages/backend/src/services/auth/controller.ts
+++ packages/backend/src/services/auth/controller.ts
@@ -2,12 +2,14 @@ import { recoverPersonalSignature } from 'eth-sig-util';
 import { bufferToHex } from 'ethereumjs-util';
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import CardanoWasm = require('@emurgo/cardano-serialization-lib-nodejs');
+import CardanoMsg = require('@emurgo/cardano-message-signing-nodejs');
 
 import { config } from '../../config';
 import { User } from '../../models/user.model';
 
 export const create = (req: Request, res: Response, next: NextFunction) => {
-	const { signature, publicAddress } = req.body;
+	const { signature, publicAddress, key } = req.body;
 	if (!signature || !publicAddress)
 		return res
 			.status(400)
@@ -41,25 +43,126 @@ export const create = (req: Request, res: Response, next: NextFunction) => {
 				}
 
 				const msg = `I am signing my one-time nonce: ${user.nonce}`;
+				switch (user.blockchain) {
+					case 'ETHEREUM': {
+						const msgBufferHex = bufferToHex(
+							Buffer.from(msg, 'utf8')
+						);
+						const address = recoverPersonalSignature({
+							data: msgBufferHex,
+							sig: signature,
+						});
 
-				// We now are in possession of msg, publicAddress and signature. We
-				// will use a helper from eth-sig-util to extract the address from the signature
-				const msgBufferHex = bufferToHex(Buffer.from(msg, 'utf8'));
-				const address = recoverPersonalSignature({
-					data: msgBufferHex,
-					sig: signature,
-				});
+						// The signature verification is successful if the address found with
+						// sigUtil.recoverPersonalSignature matches the initial publicAddress
+						if (
+							address.toLowerCase() ===
+							publicAddress.toLowerCase()
+						) {
+							return user;
+						} else {
+							res.status(401).send({
+								error: 'Signature verification failed',
+							});
+							return null;
+						}
+					}
+					case 'CARDANO': {
+						const baseAddr = CardanoWasm.BaseAddress.from_address(
+							CardanoWasm.Address.from_bytes(
+								Buffer.from(user.publicAddress, 'hex')
+							)
+						);
+						const message = CardanoMsg.COSESign1.from_bytes(
+							Buffer.from(signature, 'hex')
+						);
+						const payload = message.payload();
+						if (
+							payload &&
+							Buffer.from(msg, 'utf-8').compare(payload)
+						)
+							throw Error('Payload is not correct.');
+						const coseHeaders = message
+							.headers()
+							.protected()
+							.deserialized_headers();
+						const coseAddressBytes = coseHeaders
+							.header(CardanoMsg.Label.new_text('address'))
+							?.as_bytes();
+						if (!coseAddressBytes)
+							throw Error('no address in signature');
+						const coseAddress = CardanoWasm.Address.from_bytes(
+							coseAddressBytes
+						);
+						const cosePublicKeyBytes = coseHeaders.key_id();
+						let publicKey = undefined;
+						if (cosePublicKeyBytes) {
+							publicKey = CardanoWasm.PublicKey.from_bytes(
+								cosePublicKeyBytes
+							);
+						} else {
+							if (!key) throw Error('no key provided');
+							const coseKey = CardanoMsg.COSEKey.from_bytes(
+								Buffer.from(key, 'hex')
+							);
+							const publicKeyBytes = coseKey
+								.header(
+									CardanoMsg.Label.new_int(
+										CardanoMsg.Int.new_negative(
+											CardanoMsg.BigNum.from_str('2')
+										)
+									)
+								)
+								?.as_bytes();
+							if (!publicKeyBytes) {
+								console.log(
+									"Can't extract public key from provided key."
+								);
+								return null;
+							}
+							publicKey = CardanoWasm.PublicKey.from_bytes(
+								publicKeyBytes
+							);
+						}
 
-				// The signature verification is successful if the address found with
-				// sigUtil.recoverPersonalSignature matches the initial publicAddress
-				if (address.toLowerCase() === publicAddress.toLowerCase()) {
-					return user;
-				} else {
-					res.status(401).send({
-						error: 'Signature verification failed',
-					});
-
-					return null;
+						const paymentKeyHash = publicKey.hash();
+						const stakingKeyHash = baseAddr
+							?.stake_cred()
+							.to_keyhash();
+						if (!stakingKeyHash || !paymentKeyHash) {
+							console.log('No staking key available.');
+							return null;
+						}
+						const reconsctructedAddress = CardanoWasm.BaseAddress.new(
+							CardanoWasm.NetworkInfo.testnet().network_id(),
+							CardanoWasm.StakeCredential.from_keyhash(
+								paymentKeyHash
+							),
+							CardanoWasm.StakeCredential.from_keyhash(
+								stakingKeyHash
+							)
+						);
+						if (
+							baseAddr?.to_address().to_bech32() !==
+							reconsctructedAddress.to_address().to_bech32()
+						) {
+							console.log('Addresses are not same.');
+							return null;
+						}
+						const ed25519Sig = CardanoWasm.Ed25519Signature.from_bytes(
+							message.signature()
+						);
+						const data = message
+							.signed_data(undefined, Buffer.from(msg, 'utf-8'))
+							.to_bytes();
+						if (!publicKey.verify(data, ed25519Sig)) {
+							console.log('Signature failed to verify.');
+							return null;
+						}
+						return user;
+					}
+					default:
+						return null;
 				}
 			})
 			////////////////////////////////////////////////////
